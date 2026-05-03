f281e428502fb8605bb5e1dbb7b0244d5a6f2095
Ayu Novianingrum <ayunovianingrum.work@gmail.com>
2026-05-04 03:33:42 +0700

    Storiy Board v1.0 - fix deployment needs

--- docs/registerSW.js
+++ docs/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/storiy-board/sw.js', { scope: '/storiy-board/' })})}
